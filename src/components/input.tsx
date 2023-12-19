e2e093b2bfd1672d388e4811fff459ba89e59a1f
ViniGhiraldi <vinighiraldi198@gmail.com>
2023-12-19 13:24:06 -0300

    feat: fixed bugs

--- src/components/input.tsx
+++ src/components/input.tsx
@@ -1,5 +1,5 @@
 import { twMerge } from "tailwind-merge"
 
 export const Input = ({ className, ...rest }: React.ComponentProps<'input'>) => {
-    return <input type="text" className={twMerge('border p-2 rounded-md hover:shadow-inner transition-shadow', className)} {...rest} />
+    return <input type="text" className={twMerge('border w-full p-2 rounded-md hover:shadow-inner transition-shadow', className)} {...rest} />
 }
